7bae9459679664ae9e29814842a783780fd1a41d
Keith Kenyon <Keith Kenyon>
2016-08-11 15:16:37 -0500

    Initial commit

---
+++ app/src/main/java/com/kenyon/david/helloworld/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.kenyon.david.helloworld;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+publicbutton but1;
+    public void init(){
+        but1=(Button)findViewById(R.id.but1);
+        but1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent toy=new Intent (first.this,second.class);
+            }
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
